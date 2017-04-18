b4d70411cf4884a0c0e9243d0e1ee44c1d394a01
Marc Plouhinec <m_plouhinec@yahoo.fr>
2017-04-18 23:34:34 +0200

    Commit a fresh Spring Boot application.

---
+++ src/main/kotlin/fr/marcsworld/TrustservicereputationApplication.kt
@@ -0,0 +1,11 @@
+package fr.marcsworld
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TrustservicereputationApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TrustservicereputationApplication::class.java, *args)
+}
---
+++ src/test/kotlin/fr/marcsworld/TrustservicereputationApplicationTests.kt
@@ -0,0 +1,16 @@
+package fr.marcsworld
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TrustservicereputationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
