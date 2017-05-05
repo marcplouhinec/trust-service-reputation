f66b2649fc22ac5bf0e491b8ef2f484dcba69789
Marc Plouhinec <m_plouhinec@yahoo.fr>
2017-05-05 15:15:02 +0200

    Handle expand/collapse buttons and improve the styling.

--- src/main/resources/static/script/treeController.js
+++ src/main/resources/static/script/treeController.js
@@ -9,6 +9,19 @@ var treeController = {
      * Initialize event listeners and default display configuration.
      */
     initialize: function () {
+        // Handle the expand/collapse buttons
+        $('.expandable-tree-node > button').click(function () {
+            var $subNodes = $(this).parent().parent().children('.tree-sub-nodes');
+
+            if ($(this).text() === '-') {
+                $subNodes.hide();
+                $(this).text('+');
+            } else {
+                $subNodes.show();
+                $(this).text('-');
+            }
+        });
+
         // Hide long lists of agency names with a "show all" button
         $('table.agency-names').each(function () {
             var $agencyNameRows = $(this).find('> tbody > tr');
