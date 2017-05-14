a7d632e1dad1efed7f338ba3aad5840712f8a51d
Marc Plouhinec <m_plouhinec@yahoo.fr>
2017-05-14 18:02:20 +0200

    Update the version in the pom.xml and prepare installation scripts.

---
+++ install/build-deb-package.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#
+# Build a Debian package.
+# Usage: ./build-deb-package.sh build_number
+# Note: this script must be executed after the "mvn clean install" command.
+#
+
+if [ -z "$1" ]
+  then
+    echo "Usage: ./build-deb-package.sh build_number"
+fi
+BUILD_NUMBER=$1
+
+cd ..
+cp -r ./debian ./target/debian
+cd ./target
+
+# Set the Debian package version
+cp ./debian/DEBIAN/control.tpl ./debian/DEBIAN/control
+sed -i "s/\${version}/1.0.0.${BUILD_NUMBER}/g" ./debian/DEBIAN/control
+
+# Copy and rename the fat JAR
+rm -rf ./debian/usr/local/lib/trustservicereputation/*
+cp ./trustservicereputation-1.0.0-SNAPSHOT.jar ./debian/usr/local/lib/trustservicereputation/trustservicereputation.jar
+
+# Adjust ownerships
+fakeroot chown -R root:root debian
+fakeroot chmod +x debian/etc/init.d/trustservicereputation
+fakeroot chmod +x debian/DEBIAN/postinst
+
+# build the package
+fakeroot dpkg-deb --build debian .
+ln -s "./trustservicereputation_1.0.0.${BUILD_NUMBER}_all.deb" ./trustservicereputation_latest_all.deb
